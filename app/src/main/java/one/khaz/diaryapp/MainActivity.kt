2ed9b04c1e98119cf9481014052d32c2d7c32f14
Khairil Azmi Ashari <KhairilAzmiAshari@gmail.com>
2024-03-24 20:56:51 +0700

    add Splashscreen

---
+++ app/src/main/java/one/khaz/diaryapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package one.khaz.diaryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import one.khaz.diaryapp.ui.theme.DiaryAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            DiaryAppTheme {
+
+            }
+        }
+    }
+}
